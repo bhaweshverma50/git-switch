c92317ebf1acdc868305fd80f5e3cda3815f5ccb
Personal <bhaweshverma50@gmail.com>
2025-12-17 10:38:23 +0530

    Initial Commit

---
+++ Git Switch/Git_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_SwitchApp.swift
+//  Git Switch
+//
+//  Created by Bhawesh Verma on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Git_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
